61d017b789da9309b8810aa664234995cdd5b257
Felipe Tavares <felipe.tavares@outlook.com>
2023-08-31 22:07:59 -0300

    initial commit

---
+++ ScreenSound/Music.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound
+{
+    class Music
+    {
+        public string name;
+        public string artist;
+        public int durationTime;
+        public bool available;
+
+        public void ShowMusicInfo()
+        {
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Artist: {artist}");
+            Console.WriteLine($"Duration time: {durationTime}");
+            if ( available )
+                Console.WriteLine("Available");
+            else
+                Console.WriteLine("Not Available in your plan");
+
+        }
+    }
+}
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,16 @@
+﻿using ScreenSound;
+
+Music music1 = new Music();
+music1.name = "Roxane";
+music1.artist = "The Police";
+music1.durationTime = 270;
+music1.available = true;
+
+Music music2 = new Music();
+music2.name = "Vertigo";
+music2.artist = "U2";
+music2.durationTime = 350;
+music2.available = false;
+
+music1.ShowMusicInfo();
+music2.ShowMusicInfo();
