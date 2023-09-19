7434c26df6bb1a8d71e62d8fb6728bc250950f05
José Felipe Tavares <jose.costa@csgd.com.br>
2023-09-19 19:51:51 -0300

    Add class album

---
+++ Album.cs
@@ -0,0 +1,13 @@
+namespace ScreenSound
+{
+    class Album
+    {
+        public string Name { get; set; }
+        public int Duration { get; set; }
+
+        public void AddMusic(Musica musica)
+        {
+
+        }
+    }
+}
